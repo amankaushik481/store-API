b0a35dae580d6bc3aa625666267ddac80aca6e9f
Aman Kaushik <amankaushik481@gmail.com>
2022-06-16 14:10:09 +0530

    minor error resolved

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log('04 Store API')
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
+
+//middleware
+app.use(express.json);
+
+app.get("/", (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products">Products route</a>');
+});
+
+const port = process.env.PORT || 3000;
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const start = async () => {
+  try {
+    app.listen(port, console.log(`The Server is running on ${port} `));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
